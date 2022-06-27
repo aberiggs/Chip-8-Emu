baf559ed2d4f38893a36b4b3c570c7e4756c1c4b
aberiggs <34726295+aberiggs@users.noreply.github.com>
2022-06-26 19:52:17 -0700

    Added IBM Rom, and set up struct in header file.

--- main.c
+++ main.c
@@ -1,40 +1,40 @@
 #include "main.h"
 
+#include "include/SDL2/SDL.h"
+
 #include <stdbool.h>
 #include <stdio.h>
-#include "include/SDL2/SDL.h"
 #include <string.h>
 
 int main() {
-	printf("Welcome to Sprocket's Chip-8 Emulator...\n");
-	/*
-	if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
-		printf("Error initializing SDL %s\n", SDL_GetError());
-	}
-	*/
-	
-	SDL_CreateWindow("Chip-8", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1000, 1000,
-										SDL_WINDOW_RESIZABLE);
+  printf("Welcome to Sprocket's Chip-8 Emulator...\n");
 
-	SDL_Event e;
+  int scaling = 5;
+  int window_width = scaling * DISPLAY_WIDTH;
+  int window_height = scaling * DISPLAY_HEIGHT;
 
-	bool quit = false;
+  if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+    printf("Error initializing SDL %s\n", SDL_GetError());
+  }
 
-	while (!quit) {
-		while (SDL_PollEvent(&e)) {
-			if (e.type == SDL_QUIT) {
-				quit = true;
-			}
-			if (e.type == SDL_KEYDOWN) {
-				quit = true;
-			}
-			if (e.type == SDL_MOUSEBUTTONDOWN) {
-				quit = true;
-			}
-		}
-	}
 
-	SDL_Quit();
+  SDL_CreateWindow("Chip-8", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 
+                   window_width, window_height,
+                   SDL_WINDOW_RESIZABLE);
 
-	return 0;
+  SDL_Event e;
+
+  bool quit = false;
+
+  while (!quit) {
+    while (SDL_PollEvent(&e)) {
+      if (e.type == SDL_QUIT) {
+        quit = true;
+      }
+    }
+  }
+
+  SDL_Quit();
+
+  return 0;
 }
--- main.h
+++ main.h
@@ -6,5 +6,18 @@
 #define DISPLAY_WIDTH (64)
 #define DISPLAY_HEIGHT (32)
 
+typedef struct chip_8 {
+  unsigned short opcode;
+  unsigned char memory[RAM_SIZE];
+  unsigned char V[16];
+  unsigned short I;
+  unsigned short pc;
+  unsigned char gfx[DISPLAY_WIDTH * DISPLAY_HEIGHT];
+  unsigned char delay_timer;
+  unsigned char sound_timer;
+  unsigned short stack[16];
+  unsigned short sp;
+  unsigned char key[16];
+} ch8_t;
 
 #endif
